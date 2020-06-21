df9e510130adac35f816a590840e624361cd67f2
Slizzygod <Slizzygod@mail.ru>
2020-06-21 07:48:15 +0300

    Eto tochno vse

--- src/components/CardRating.js
+++ src/components/CardRating.js
@@ -9,8 +9,8 @@ function CardRating() {
     <CardGrid >
       <Card size = "l" mode = "outline" >
       <RichCell disabled multiline before = {
-        <Avatar size = {72} src={('https://thispersondoesnotexist.com/image')}/>}
-          text = "Тарас Иванов"
+        <Avatar size = {72} src={('https://sun1-90.userapi.com/4Vs_tQxNFmbfsCu_Fm0_DesTY6URSgqiJRBkTg/3Aq6E84R73E.jpg')}/>}
+          text = "Михаил Козлов"
           caption = "Легаси"
           after = "5000 дублонов"
           actions = {
@@ -21,8 +21,8 @@ function CardRating() {
       </Card> 
       <Card size = "l" mode = "outline" >
       <RichCell disabled multiline before = {
-        <Avatar size = {72} src={('https://thispersondoesnotexist.com/image')}/>}
-          text = "Иванова Лиза"
+        <Avatar size = {72} src={('https://sun9-22.userapi.com/c854532/v854532193/498b/x4Q5jG76y34.jpg')}/>}
+          text = "Александр Пирожников"
           caption = "Опытный боец"
           after = "2100 дублонов"
           actions = {
@@ -33,8 +33,8 @@ function CardRating() {
       </Card>  
       <Card size = "l" mode = "outline" >
       <RichCell disabled multiline before = {
-        <Avatar size = {72} src={('https://thispersondoesnotexist.com/image')}/>}
-          text = "Иванов Данила"
+        <Avatar size = {72} src={('https://sun9-21.userapi.com/c630216/v630216429/1d8dc/awutUzNGrCA.jpg')}/>}
+          text = "Александр Павлов"
           caption = "Новичок"
           after = "1200 дублонов"
           actions = {
@@ -45,8 +45,8 @@ function CardRating() {
       </Card>
       <Card size = "l" mode = "outline" >
       <RichCell disabled multiline before = {
-        <Avatar size = {72} src={('https://thispersondoesnotexist.com/image')}/>}
-          text = "Шымов Юлия"
+        <Avatar size = {72} src={('https://sun9-41.userapi.com/c858336/v858336107/10aaca/Y0FWWQmjJ88.jpg')}/>}
+          text = "Владислав Захаров"
           caption = "Опытный боец"
           after = "500 дублонов"
           actions = {
@@ -57,8 +57,8 @@ function CardRating() {
       </Card>
       <Card size = "l" mode = "outline" >
       <RichCell disabled multiline before = {
-        <Avatar size = {72} src={('https://thispersondoesnotexist.com/image')}/>}
-          text = "Кузнецов Киря"
+        <Avatar size = {72} src={('https://sun1-85.userapi.com/vn-4b8D3PvLkx1lTnrxFaB4fV6GVAd3SZS--8w/twmsYU7nrII.jpg')}/>}
+          text = "Тимур Токарев"
           caption = "Новичок"
           after = "500 дублонов"
           actions = {
